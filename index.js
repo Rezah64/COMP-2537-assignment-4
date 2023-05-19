6ca33daabc3f11c028e7fe6f81c99ad47b9e895d
Reza Hedieloo <reza_hedieloo@yahoo.com>
2023-05-19 08:22:24 -0700

    Debug

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ let seconds = 0; // variable to hold the number of seconds elapsed
 let images = []; // array to store the retrieved Pokémon images
 let firstCard = undefined;
 let secondCard = undefined;
-let rows = 2; // default number of rows
-let cols = 3; // default number of columns
+let rows = 0; // default number of rows
+let cols = 0; // default number of columns
+let totalPairs = Math.ceil(rows * cols / 2);
+let matchedPairs = 0;
+let pairsLeft = totalPairs;
+let clicksCount = 0;
 
 const startTimer = () => {
   gameTimer = setInterval(() => {
     seconds++;
-    $('#timer').text(`Timer: ${seconds} seconds`);
+    $('#timer').text(`${seconds} `);
   }, 1000);
 };
 
@@ -17,6 +21,10 @@ const resetTimer = () => {
   clearInterval(gameTimer);
   seconds = 0;
   $('#timer').text('Timer: 0 seconds');
+  matchedPairs = 0;
+  pairsLeft = totalPairs;
+  clicksCount = 0;
+  updateStats();
 };
 
 const retrievePokemonImages = () => {
@@ -64,6 +72,7 @@ const shuffleArray = (array) => {
 
   return shuffledArray;
 };
+
 const createGameGrid = () => {
   const gameGrid = $('#game_grid');
 
@@ -85,14 +94,14 @@ const createGameGrid = () => {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       const card = $('<div>').addClass('card').css('width', cardWidth);
-      const frontFace = $('<img>').addClass('front_face')
+      const frontFace = $('<img>').addClass('front_face');
       const backFace = $('<img>').addClass('back_face').attr('src', 'back.webp');
 
       card.append(frontFace).append(backFace);
       gameGrid.append(card);
     }
   }
-  
+
   images = shuffleArray(images);
 
   const cards = gameGrid.find('.card');
@@ -103,8 +112,19 @@ const createGameGrid = () => {
 
   setup();
 };
+
+const updateStats = () => {
+  $('#totalPairs').text(totalPairs);
+  $('#matchedPairs').text(matchedPairs);
+  $('#pairsLeft').text(pairsLeft);
+  $('#clicksCount').text(clicksCount);
+};
+
 const setup = () => {
+  
   $(".card").on("click", function () {
+    clicksCount++;
+    updateStats();
     $(this).toggleClass("flip");
 
     if (!firstCard) {
@@ -116,6 +136,20 @@ const setup = () => {
         console.log("match");
         firstCard = undefined;
         secondCard = undefined;
+
+        matchedPairs++;
+        pairsLeft--;
+        // clicksCount++;
+        updateStats();
+
+        if (matchedPairs === totalPairs) {
+          clearInterval(gameTimer);
+          alert("Congratulations! You've won the game!");
+        }
+        if (seconds === 100) {
+          clearInterval(gameTimer);
+          alert("Time's up! You've lost the game!");
+        }
       } else {
         console.log("no match");
         setTimeout(() => {
@@ -124,12 +158,16 @@ const setup = () => {
           firstCard = undefined;
           secondCard = undefined;
         }, 1000);
+        // clicksCount++;
+        updateStats();
       }
     } else {
       $(firstCard).toggleClass("flip");
       $(secondCard).toggleClass("flip");
       firstCard = this;
       secondCard = undefined;
+      // clicksCount++;
+      updateStats();
     }
   });
 
@@ -146,20 +184,29 @@ const setup = () => {
   $('#easy-btn').on('click', () => {
     rows = 2;
     cols = 3;
+    totalPairs = Math.ceil(rows * cols / 2);
+    resetTimer();
     createGameGrid();
   });
 
   $('#medium-btn').on('click', () => {
     rows = 3;
     cols = 4;
+    totalPairs = Math.ceil(rows * cols / 2);
+    resetTimer();
     createGameGrid();
   });
 
   $('#hard-btn').on('click', () => {
     rows = 4;
     cols = 6;
+    totalPairs = Math.ceil(rows * cols / 2);
+    resetTimer();
     createGameGrid();
   });
 };
 
-$(document).ready(setup)
+$(document).ready(() => {
+  updateStats();
+  setup();
+});
